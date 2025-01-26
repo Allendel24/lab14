9b5f8fefad50993e51a2ed6dc4ba2587cd35ebce
Film_ <pawich23013@gmail.com_>
2025-01-26 16:47:12 +0700

    lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
+void swap(T d[], int x, int y){
+    if(d[y] > d[x]){
+        T S = d[x];
+        d[x] = d[y];
+        d[y] = S;
+    }
+}
 
+template <typename T>
+void insertionSort(T d[],int N){
+    for(int i = 1; i < N; i++){
+        for(int j = i; j > 0; j--){
+            swap(d, j - 1, j);
+        }
+        cout << "Pass " << i << ":";
+        for(int k = 0; k < N; k++){
+            cout << d[k] << " ";
+        }
+        cout << endl;
+    }
 }
 
 int main(){
